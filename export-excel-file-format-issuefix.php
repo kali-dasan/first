2466c00795ce8c4100fab62de2bb0051d73859fa
kalidasan <kalidasan.seetharaman@capgemini.com>
2015-11-26 16:07:21 +0530

    kali:file open issue

--- export-excel-file-format-issuefix.php
+++ export-excel-file-format-issuefix.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * Implements function to export data into xls file.
  */
 function download_xls() {
@@ -25,6 +25,33 @@ function download_xls() {
     // Push each row contents.
     echo implode("\t", $row) . "\n";
   }
-  exit;
+  // Phpexcel is a drupal module to do export / create xls files.
+  // Below is the example code to export xls file with out any open file format issue.
+  module_load_include('inc', 'phpexcel');
+  // Create directory my_dir.
+  $filepath = 'public://my_dir/';
+  file_prepare_directory($filepath, FILE_CREATE_DIRECTORY);
+
+  // Store file.
+  $dir = file_stream_wrapper_get_instance_by_uri('public://my_dir')->realpath();
+  $filename_timestamp = "file-name" . date('d-m-Y') . ".xls";
+  $path = "$dir/$filename_timestamp";
+
+  // Use the .xls format
+  $options = array('format' => 'xls');
+  $excel_result = phpexcel_export($header, $data, $path, $options);
+
+  // To avoid file format issue while open file.
+  $file = new stdClass();
+  $file->uri = $path;
+
+  header('Content-Type: application/vnd.ms-excel');
+  header('Content-Disposition: attachment; filename="' . $filename_timestamp . '"');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate');
+  header('Pragma: public');
+
+  readfile($file->uri);
+  exit
 }
 ?>
